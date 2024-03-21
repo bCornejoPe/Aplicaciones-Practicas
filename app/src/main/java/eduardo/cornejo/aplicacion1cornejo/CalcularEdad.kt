c9114c0087e85a3119f2fdb673a6f09a0baeb171
Bryan cornejo <20230223@ricaldone.edu.sv>
2024-03-21 11:48:31 -0600

    Primera Aplicacion

---
+++ app/src/main/java/eduardo/cornejo/aplicacion1cornejo/CalcularEdad.kt
@@ -0,0 +1,9 @@
+package eduardo.cornejo.aplicacion1cornejo
+
+class CalcularEdad {
+   fun resta (Num1: Int): Int {
+        val edad= 2024-Num1
+       return edad
+   }
+
+}
---
+++ app/src/main/java/eduardo/cornejo/aplicacion1cornejo/MainActivity.kt
@@ -0,0 +1,35 @@
+package eduardo.cornejo.aplicacion1cornejo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val txtNum1 = findViewById<EditText>(R.id.txtNum1)
+        val btnCalcularEdad= findViewById<Button>(R.id.btnCalcular)
+        val txtEdad = findViewById<TextView>(R.id.txtEdad)
+
+         val objCalcularEdad= CalcularEdad()
+
+         btnCalcularEdad.setOnClickListener {
+             var edad=  objCalcularEdad.resta(txtNum1.text.toString().toInt())
+
+             txtEdad.text= "El resltado es $edad"
+         }
+    }
+}
